c81d58ddaaeee2af2f3c409eb0c4dfbf78506512
Andrew Bevitt <andrew@andrewbevitt.com>
2013-11-27 09:42:11 +0900

    add checks for other  array keys

--- install/srv/www/default/public/helloworld.php
+++ install/srv/www/default/public/helloworld.php
@@ -7,8 +7,8 @@
 	a site or domain administrator check your DNS settings.</p>
 	<ul>
 		<li>Referer: <?php print(array_key_exists('HTTP_REFERER', $_SERVER) ? $_SERVER['HTTP_REFERER']: 'none'); ?></li>
-		<li>Your IP: <?php print($_SERVER['REMOTE_ADDR']); ?></li>
-		<li>Browser: <?php print($_SERVER['HTTP_USER_AGENT']); ?></li>
+		<li>Your IP: <?php print(array_key_exists('REMOTE_ADDR', $_SERVER) ? $_SERVER['REMOTE_ADDR'] : 'unknown'); ?></li>
+		<li>Browser: <?php print(array_key_exists('HTTP_USER_AGENT', $_SERVER) ? $_SERVER['HTTP_USER_AGENT'] : 'unknown'); ?></li>
 	</ul>
 	<p>If you are browsing a <strong>SSL</strong> secured site and you are sure the
 	address is correct then you are probably browsing with a client that does not
